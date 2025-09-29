4ca6bd306d042e61ffd2aab7091900afd305b7b6
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-29 21:12:54 +0530

    special_variable

--- special_variables.sh
+++ special_variables.sh
@@ -4,3 +4,5 @@ echo "All the variables passed: $@"  #$@ or $* represents all the in bash script
 echo "All the variables passed: $*"
 echo "script name: $0" #$0 used to get script name in bash
 echo "current directory: $PWD" #$PWD helps to get current directory
+echo "who is running: $USER"
+echo "home directory: $HOME"
