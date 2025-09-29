513e7c3c63a8a3ce0b4ca1f40aea361cfe8aaf23
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-29 21:09:38 +0530

    special_variable

--- special_variables.sh
+++ special_variables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "All the variables passed: $@"
+echo "All the variables passed: $@"  #$@ or $* represents all the in bash script
 echo "All the variables passed: $*"
+echo "script name: $0" #$0 used to get script name in bash
