35e3d812f194d27d122c1309f017d6c83cbbd5b7
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-30 18:56:26 +0530

    installation_functions

---
+++ installation123.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "pleae run this script as root user"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation failed"
+        exit 1
+    else
+        echo "$2 installed successfully"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
+
+dnf install nginx -y
+
+VALIDATE $? "nginx"
+
+dnf install mongodb-mongosh -y
+
+VALIDATE $? "mongosh"
