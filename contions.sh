ec5b6aa2e6e222af6282f1769642b1617f4ddacc
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-30 18:08:32 +0530

    conditions

---
+++ contions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+Number=$1
+if [ $Number -lt 10 ]; then
+    echo "The number is less than 10"
+esle
+    echo "The number is 10 or greater"
+fi
+
+#-lt less than
+#-le less than or equal to
+#-gt greater than
+#-ge greater than or equal to
+#-eq equal to
+#-ne not equal to   
