cf9468e60df35d91d62b99fd8163ad233b9599ec
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-30 19:30:34 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+LOGS_FOLDER="/var/log/shell-scripts"
+SCRIPT_NAME=$( $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started at $(date)" >>$LOG_FILE
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo -e " $g pleae run this script as root user $n"
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e " $r $2 installation failed $n"
+        exit 1
+    else
+        echo -e " $g $2 installed successfully $n"
+    fi
+}
+
+dnf install mysql -y &>>LOG_FILE # install mysql
+if [ $? -ne 0 ]; then
+    echo -e " $r mysql installation failed $n"
+    VALIDATE $? "mysql"
+else
+    echo -e " $g mysql installed successfully $n"
+fi
+
+
+dnf install nginx -y &>>LOG_FILE
+if [ $? -ne 0 ]; then
+    echo -e " $r nginx installation failed $n"
+    VALIDATE $? "nginx"
+else
+    echo -e " $g nginx installed successfully $n"
+fi
+
+dnf install python3 -y &>>LOG_FILE
+if [ $? -ne 0 ]; then
+    echo -e " $r python3 installation failed $n"
+    VALIDATE $? "python3"
+else
+    echo -e " $g python3 installed successfully $n"
+fi
