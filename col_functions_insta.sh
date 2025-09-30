c3e0616e5469ab1c731345315e393fdb3b374d09
gopikrishna <gopikrishnamaneem@gmail.com>
2025-09-30 19:17:59 +0530

    colors

--- col_functions_insta.sh
+++ col_functions_insta.sh
@@ -7,40 +7,40 @@ n="\e[0m"
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-    echo " $g pleae run this script as root user $n"
+    echo -e " $g pleae run this script as root user $n"
     exit 1
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo " $r $2 installation failed $n"
+        echo -e " $r $2 installation failed $n"
         exit 1
     else
-        echo " $g $2 installed successfully $n"
+        echo -e " $g $2 installed successfully $n"
     fi
 }
 
 dnf install mysql -y  # install mysql
 if [ $? -ne 0 ]; then
-    echo " $r mysql installation failed $n"
+    echo -e " $r mysql installation failed $n"
     VALIDATE $? "mysql"
 else
-    echo " $g mysql installed successfully $n"
+    echo -e " $g mysql installed successfully $n"
 fi
 
 
 dnf install nginx -y
 if [ $? -ne 0 ]; then
-    echo " $r nginx installation failed $n"
+    echo -e " $r nginx installation failed $n"
     VALIDATE $? "nginx"
 else
-    echo " $g nginx installed successfully $n"
+    echo -e " $g nginx installed successfully $n"
 fi
 
 dnf install python3 -y
 if [ $? -ne 0 ]; then
-    echo " $r python3 installation failed $n"
+    echo -e " $r python3 installation failed $n"
     VALIDATE $? "python3"
 else
-    echo " $g python3 installed successfully $n"
+    echo -e " $g python3 installed successfully $n"
 fi
